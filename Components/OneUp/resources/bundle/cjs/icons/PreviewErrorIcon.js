3f649b623bd48358ef13097bda49b351b7723124
MrTristin2 <138416840+MrTristin2@users.noreply.github.com>
2023-09-03 21:54:10 -0500

    Add files via upload

---
+++ Components/OneUp/resources/bundle/cjs/icons/PreviewErrorIcon.js
@@ -0,0 +1,35 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+var _react = _interopRequireDefault(require("react"));
+
+var _Icon = _interopRequireDefault(require("@react/react-spectrum/Icon"));
+
+var _preview_error = _interopRequireDefault(require("./preview_error.svg"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+/*************************************************************************
+ * ADOBE CONFIDENTIAL
+ * ___________________
+ *
+ *  Copyright 2019 Adobe
+ *  All Rights Reserved.
+ *
+ * NOTICE:  All information contained herein is, and remains
+ * the property of Adobe and its suppliers, if any. The intellectual
+ * and technical concepts contained herein are proprietary to Adobe
+ * and its suppliers and are protected by all applicable intellectual
+ * property laws, including trade secret and copyright laws.
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from Adobe.
+ **************************************************************************/
+const PreviewErrorIcon = () => /*#__PURE__*/_react.default.createElement(_Icon.default, null, /*#__PURE__*/_react.default.createElement(_preview_error.default, null));
+
+var _default = PreviewErrorIcon;
+exports.default = _default;
