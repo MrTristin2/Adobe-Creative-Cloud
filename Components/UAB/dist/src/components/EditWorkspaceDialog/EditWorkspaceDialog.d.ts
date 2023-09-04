a7d951ad129086294dae694d3cef154b13988fdd
MrTristin2 <138416840+MrTristin2@users.noreply.github.com>
2023-09-04 10:56:40 -0500

    Add files via upload

---
+++ Components/UAB/dist/src/components/EditWorkspaceDialog/EditWorkspaceDialog.d.ts
@@ -0,0 +1,3 @@
+import type { InternalInitOptions } from '../../types/InitOptions';
+export default function registerEditWorkspaceDialog(config: InternalInitOptions): void;
+//# sourceMappingURL=EditWorkspaceDialog.d.ts.map
