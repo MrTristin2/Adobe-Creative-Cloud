2282e3fd54c3d23162f603c703fc155107333921
MrTristin2 <138416840+MrTristin2@users.noreply.github.com>
2023-09-03 22:42:22 -0500

    Add files via upload

---
+++ Components/OneUp/resources/bundle/cjs/preview/react-component/header/presentation/separator/SeparatorView.js
@@ -0,0 +1,35 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+var _react = _interopRequireDefault(require("react"));
+
+require("!style-loader!css-loader!./SeparatorView.css");
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+/*************************************************************************
+* ADOBE CONFIDENTIAL
+* ___________________
+*
+* Copyright 2020 Adobe
+* All Rights Reserved.
+*
+* NOTICE: All information contained herein is, and remains
+* the property of Adobe and its suppliers, if any. The intellectual
+* and technical concepts contained herein are proprietary to Adobe
+* and its suppliers and are protected by all applicable intellectual
+* property laws, including trade secret and copyright laws.
+* Dissemination of this information or reproduction of this material
+* is strictly forbidden unless prior written permission is obtained
+* from Adobe.
+**************************************************************************/
+const SeparatorView = () => /*#__PURE__*/_react.default.createElement("div", {
+  className: "sdk-SeparatorView-separator"
+});
+
+var _default = SeparatorView;
+exports.default = _default;
