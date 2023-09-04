f4b6968bb6a86e43f5fdf2c8104cd4fac12ab95c
MrTristin2 <138416840+MrTristin2@users.noreply.github.com>
2023-09-03 21:53:30 -0500

    Create ContinuousIcon.js

---
+++ Components/OneUp/resources/bundle/cjs/icons/ContinuousIcon.js
@@ -0,0 +1,35 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+var _react = _interopRequireDefault(require("react"));
+
+var _Icon = _interopRequireDefault(require("@react/react-spectrum/Icon"));
+
+var _s_continuouspages_ = _interopRequireDefault(require("./s_continuouspages_22.svg"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+/*************************************************************************
+ * ADOBE CONFIDENTIAL
+ * ___________________
+ *
+ *  Copyright 2019 Adobe
+ *  All Rights Reserved.
+ *
+ * NOTICE:  All information contained herein is, and remains
+ * the property of Adobe and its suppliers, if any. The intellectual
+ * and technical concepts contained herein are proprietary to Adobe
+ * and its suppliers and are protected by all applicable intellectual
+ * property laws, including trade secret and copyright laws.
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from Adobe.
+ **************************************************************************/
+const ContinuousIcon = () => /*#__PURE__*/_react.default.createElement(_Icon.default, null, /*#__PURE__*/_react.default.createElement(_s_continuouspages_.default, null));
+
+var _default = ContinuousIcon;
+exports.default = _default;
