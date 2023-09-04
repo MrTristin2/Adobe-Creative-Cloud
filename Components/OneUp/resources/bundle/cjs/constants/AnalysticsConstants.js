ced6224bf63ced4fef32ecccba6002241f07f9a8
MrTristin2 <138416840+MrTristin2@users.noreply.github.com>
2023-09-03 21:51:30 -0500

    Create AnalysticsConstants.js

---
+++ Components/OneUp/resources/bundle/cjs/constants/AnalysticsConstants.js
@@ -0,0 +1,34 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+/*************************************************************************
+* ADOBE CONFIDENTIAL
+* ___________________
+*
+*  Copyright 2020 Adobe
+*  All Rights Reserved.
+*
+* NOTICE:  All information contained herein is, and remains
+* the property of Adobe and its suppliers, if any. The intellectual
+* and technical concepts contained herein are proprietary to Adobe
+* and its suppliers and are protected by all applicable intellectual
+* property laws, including trade secret and copyright laws.
+* Dissemination of this information or reproduction of this material
+* is strictly forbidden unless prior written permission is obtained
+* from Adobe.
+**************************************************************************/
+const AnalyticsConstants = {
+  FILE_PREVIEW: "filePreview",
+  BRAND_AREA: "brandArea",
+  SIDE_MENU_CALL_TO_ACTION: "sideMenuCallToAction",
+  HELP_ICON: "helpIcon",
+  LEGAL_NOTICES: "legalNotices",
+  HUD_BRAND_AREA: "hudBrandArea",
+  FLOATING_BRAND_AREA: "floatingBrandArea"
+};
+var _default = AnalyticsConstants;
+exports.default = _default;
